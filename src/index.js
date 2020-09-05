f73dffcc4687bb39b0bd54fcd55b1079ae7a5e3f
joshmurr <joshuadavidmurr@gmail.com>
2020-09-05 14:12:54 +0100

    Combine JS files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import * as tf from '@tensorflow/tfjs';
-import { preprocessImageCV2 } from './image_processing.js';
-//import * as utils from 'utils.js';
 
 const MODELS = {
   'Flowers_Uncompressed_H5' : 'models/h5_graph/model.json',
@@ -352,6 +350,40 @@ function processImage(imgElement, outputCanvas){
 
   img.delete();
   dst.delete();
+}
+
+function preprocessImageCV2(cv2mat, dst, size, targetSize=256, downUpscale, greyscale, brightness, desaturate){
+  let output = cv2mat.clone();
+
+  if(size[0] !== 256 || size[1] !== 256){
+    const offset_x = Math.floor((size[0]- targetSize) / 2);
+    const offset_y = Math.floor((size[1] - targetSize) / 2);
+    const mask = new cv.Rect(offset_x, offset_y, targetSize, targetSize);
+
+    output = output.roi(mask);
+  }
+
+  const smallSize = Math.floor(targetSize / 24);
+  const dsize = new cv.Size(smallSize, smallSize);
+  const usize = new cv.Size(targetSize, targetSize);
+
+  if(downUpscale){
+    cv.resize(output, output, dsize, 0, 0, cv.INTER_AREA); // Downscale
+    cv.resize(output, output, usize, 0, 0, cv.INTER_CUBIC); // Upscale
+  }
+  if(desaturate){
+    cv.threshold(output, output, 150, 200, cv.THRESH_TRUNC); // Desaturate
+    //cv.cvtColor(dst, dst, cv.COLOR_RGBA2GRAY, 3); // Grayscale
+  }
+  if(brightness){
+    cv.convertScaleAbs(output, output, 2, 75);
+  }
+  if(greyscale){
+    cv.cvtColor(output, output, cv.COLOR_RGB2GRAY, 0);
+  }
+  //cv.imshow(outputCV2Video, dst);
+  output.copyTo(dst);
+  output.delete();
 }
 
 modelBtn.addEventListener('click', e => {
